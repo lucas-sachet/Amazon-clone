21aeeac4d858171740276ef97d59e05bdb95d85c
Lucas Sachet <lucasachet@gmail.com>
2021-05-30 17:13:31 -0300

    feat(front): create header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
@@ -7,7 +8,7 @@ export default function Home() {
         <title>Amazon 2.0</title>
       </Head>
 
-      <h1>Hello</h1>
+      <Header />
     </div>
   );
 }
